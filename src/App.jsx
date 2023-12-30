c8df10c1b0be6b5109671b4dc46c4af4a1672c21
Santi1994r <santiagoruiz9416@gmail.com>
2023-12-30 02:59:05 -0300

    create new app for sales value

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [porcentaje, setPorcentaje] = useState("");
+  const [monto, setMonto] = useState("");
+  const [total, setTotal] = useState("");
+
+  const calculoDeVenta = (e) => {
+    /* if (e.target.name === 'porcentaje') {
+      setPorcentaje(parseInt(e.target.value));
+    } else if (e.target.name === 'monto') {
+      setMonto(parseFloat(e.target.value));
+    } */
+
+    if (
+      
+      porcentaje >= 0 &&
+      porcentaje <= 1000 &&
+      
+      monto >= 0
+    ) {
+      const resultado = (monto * porcentaje) / 100;
+      return setTotal(resultado.toFixed(2)); // Ajusta la cantidad de decimales según tus necesidades.
+    }
+  };
+
+  /*   const calculoDeVenta = (e) => {
+    setPorcentaje(e.target.value)
+    setMonto(e.target.value)
+    if( 0 < porcentaje < 1000 && 0 < monto) {
+
+    }
+  } */
+
+  return (
+    <div className=" h-screen bg-slate-100">
+      <nav className=" bg-blue-200 w-screen h-2/6 flex flex-col justify-center items-center md:flex md:flex-row md:justify-between md:items-center px-20">
+        <img src="./images/logo-suterh.webp" alt="suterh logo" />
+        <div className=" text-blue-800 font-bold text-3xl">Bienvenidos!!</div>
+      </nav>
+      <section className=" h-4/6 flex-col px-4">
+        <div className=" flex items-center justify-center h-2/6">
+          <p className=" font-semibold text-blue-950 text-2xl">
+            Ingrese el porcentaje que quiere averiguar:{" "}
+          </p>
+          <input
+            name="porcentaje"
+            value={porcentaje}
+            type="number"
+            className=" h-8 w-24 bg-blue-100 rounded-lg px-3"
+          />
+        </div>
+        <div className=" flex items-center justify-center h-2/6">
+          <p className=" font-semibold text-blue-950 text-2xl">
+            Ingrese el monto que quiere calcular:{" "}
+          </p>
+          <input
+            name="monto"
+            value={monto}
+            type="number"
+            className=" h-8 w-24 bg-blue-100 rounded-lg px-3"
+          />
+        </div>
+        {total ? (
+          (<div className=" flex items-center justify-center h-1/6">
+            <p className=" font-bold text-red-500 text-3xl">
+              <span className=" text-red-800">Valor de venta: </span>${total}
+            </p>
+          </div>)
+          (porcentaje === "" || monto === "")(
+            <div className=" flex items-center justify-center h-1/6">
+              <p className=" font-bold text-red-500 text-3xl">
+                Tienes que poner los datos para poder calcular
+              </p>
+            </div>
+          )) 
+          : (
+          <div className=" flex items-center justify-center h-1/6">
+            <p className=" font-bold text-red-500 text-3xl">
+              Uno de los campos no coincide con lo requerido.
+            </p>
+          </div>
+        )}
+
+        <div className=" flex items-center justify-center h-1/6">
+          <button
+            onClick={calculoDeVenta}
+            className=" bg-blue-950 hover:bg-blue-900 text-white font-bold text-xl py-2 px-3 rounded-lg"
+          >
+            Calcular
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
